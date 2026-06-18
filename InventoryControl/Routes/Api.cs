79b90f7e67e7163126cfa70bcea18774cfae10af
zuxua23 <kewajibanku23@gmail.com>
2026-06-18 21:51:59 +0700

    Update Api.cs

--- InventoryControl/Routes/Api.cs
+++ InventoryControl/Routes/Api.cs
@@ -146,5 +146,16 @@ public static class Api
             pattern: "/api/search-item/{code}",
             defaults: new { controller = "SearchItem", action = "GetDetail" })
             .WithMetadata(new HttpMethodMetadata(new[] { "GET" }));
+        app.MapControllerRoute(
+            name: "api-register-with-item",
+            pattern: "/api/tag/register-with-item",
+            defaults: new { controller = "PrintTagRegis", action = "RegisterWithItem" })
+            .WithMetadata(new HttpMethodMetadata(new[] { "POST" }));
+    
+        app.MapControllerRoute(
+            name: "api-item-get",
+            pattern: "/api/item",
+            defaults: new { controller = "ItemApi", action = "Get" })
+            .WithMetadata(new HttpMethodMetadata(new[] { "GET" }));
     }
 }
